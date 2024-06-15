812b50d3bd70f298bd3c8eaef7e38e1236670b3b
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-15 14:06:40 +0700

    Todo list2

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,10 @@
+import classNames from "classnames/bind";
+import styles from "./Todo.module.scss";
+
+const cx = classNames.bind(styles);
+
+const Todo = () => {
+  return <div>Todo</div>;
+};
+
+export default Todo;
